e619bcff74429cb45c4fe591672ccd18ed402cf2
Venkat Kandagaddala <venky.kandagaddala@gmail.com>
2024-11-13 16:58:08 +0530

    teachers note tab

--- library/ws-widget/collection/src/lib/_common/content-toc/app-toc-teachers-notes/app-toc-teachers-notes.component.ts
+++ library/ws-widget/collection/src/lib/_common/content-toc/app-toc-teachers-notes/app-toc-teachers-notes.component.ts
@@ -7,11 +7,35 @@ import { Component, OnInit } from '@angular/core'
 })
 
 export class AppTocTeachersNotesComponent implements OnInit {
+
+  teacherNotes: any = [
+    {
+      title: "W3schools PDF 1",
+      url: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"
+    },
+    {
+      title: "Antennahouse PDF 2",
+      url: "https://www.antennahouse.com/hubfs/xsl-fo-sample/pdf/basic-link-1.pdf"
+    },
+    {
+      title: "Adobe PDF sample file",
+      url: "https://www.adobe.com/support/products/enterprise/knowledgecenter/media/c4611_sample_explain.pdf"
+    }
+  ]
   constructor(
   ) {
+
     
   }
 
   ngOnInit() {}
+
+  downloadPDF(object: any) {
+    console.log("Object", object)
+    const link = document.createElement('a')
+    link.href = object.url
+    link.download = `${object.title}.pdf`
+    link.click()
+  }
 
 }
