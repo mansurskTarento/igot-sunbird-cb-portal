7511333174ab78f0b4fe5914ea6475b876be3477
Sohith Kanala <kanala.sohith@tarento.com>
2024-12-09 22:20:42 +0530

    Added registration link status api

--- src/app/routes/public/public-signup/organization.resolver.ts
+++ src/app/routes/public/public-signup/organization.resolver.ts
@@ -9,11 +9,13 @@ import { switchMap, map, catchError } from 'rxjs/operators';
 import { SignupService } from './signup.service';
 import _ from 'lodash';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AppPublicOrganizationResolver implements Resolve<any> {
+export class AppPublicOrganizationResolver
+  implements Resolve<Observable<{ organizationDetails: any; designationsList: any[] }>> {
   constructor(
     private signupSvc: SignupService,
     private snackBar: MatSnackBar
@@ -28,59 +30,70 @@ export class AppPublicOrganizationResolver implements Resolve<any> {
       return of({ organizationDetails: null, designationsList: [] });
     }
 
-    return this.signupSvc.getOrgReadData(orgId).pipe(
-      switchMap((organizationDetails: any) => {
-        if (!organizationDetails) {
+    const registrationLink = `https://${environment.sitePath}/crp/${route.params.qrCodeId}/${route.params.orgId}`;
+
+    return this.signupSvc.getRegistrationLinkStatus({ registrationLink }).pipe(
+      switchMap((response: any) => {
+        if (
+          !response ||
+          response.responseCode !== 'OK' ||
+          response?.params?.errmsg !== 'Registration link is active'
+        ) {
           this.snackBar.open(
-            'Oops! The registration link seems to be invalid. Please double-check the link or request a new one.',
+            'Registration link is invalid or expired. Please reach out to your department MDO or write to us at mission.karmayogi@gov.in with organization and designation name.',
             'X',
-            {duration: 20000, panelClass: ['error']}
+            { duration: 20000, panelClass: ['error'] }
           );
           return of({ organizationDetails: null, designationsList: [] });
         }
-        if (!organizationDetails.frameworkid) {
-          this.snackBar.open(
-            'Designations not available for this organization',
-            'X', 
-            {duration: 20000, panelClass: ['error']}
-          );
-          return of({ organizationDetails, designationsList: [] });
-        }
 
-        return this.signupSvc
-          .getFrameworkInfo(organizationDetails.frameworkid)
-          .pipe(
-            map((frameworkResponse: any) => {
-              const frameworkDetails = _.get(frameworkResponse, 'result.framework');
-              const categoriesOfFramework = _.get(frameworkDetails, 'categories', []);
-              const organisationsList = this.getTermsByCode(categoriesOfFramework, 'org');
-              const disOrderedList = _.get(organisationsList, '[0].children', []);
-              const designationsList = _.sortBy(disOrderedList, 'name');
-
-              return {
-                organizationDetails,
-                designationsList,
-              };
-            }),
-            catchError((error) => {
-              if (error.error.params.errmsg) {
-                this.snackBar.open(error.error.params.errmsg, 'X', {
-                  duration: 20000,
-                  panelClass: ['error'] 
-                });
-              }
+        return this.signupSvc.getOrgReadData(orgId).pipe(
+          switchMap((organizationDetails: any) => {
+            if (!organizationDetails) {
+              this.snackBar.open(
+                'Oops! The registration link seems to be invalid. Please double-check the link or request a new one.',
+                'X',
+                { duration: 20000, panelClass: ['error'] }
+              );
               return of({ organizationDetails: null, designationsList: [] });
-            })
-          );
+            }
+
+            if (!organizationDetails.frameworkid) {
+              this.snackBar.open(
+                'Designations not available for this organization',
+                'X',
+                { duration: 20000, panelClass: ['error'] }
+              );
+              return of({ organizationDetails, designationsList: [] });
+            }
+
+            return this.signupSvc.getFrameworkInfo(organizationDetails.frameworkid).pipe(
+              map((frameworkResponse: any) => {
+                const frameworkDetails = _.get(frameworkResponse, 'result.framework');
+                const categoriesOfFramework = _.get(frameworkDetails, 'categories', []);
+                const organisationsList = this.getTermsByCode(categoriesOfFramework, 'org');
+                const disOrderedList = _.get(organisationsList, '[0].children', []);
+                const designationsList = _.sortBy(disOrderedList, 'name');
+
+                return {
+                  organizationDetails,
+                  designationsList,
+                };
+              }),
+              catchError((error) => {
+                this.handleError(error, 'Framework information could not be fetched.');
+                return of({ organizationDetails: null, designationsList: [] });
+              })
+            );
+          }),
+          catchError((error) => {
+            this.handleError(error, 'Organization information could not be fetched.');
+            return of({ organizationDetails: null, designationsList: [] });
+          })
+        );
       }),
       catchError((error) => {
-        if (error.error.params.errmsg) {
-          this.snackBar.open(error.error.params.errmsg, 'X', {
-            duration: 20000,
-            panelClass: ['error'] 
-          });
-        }
-
+        this.handleError(error, 'Error validating registration link.');
         return of({ organizationDetails: null, designationsList: [] });
       })
     );
@@ -91,5 +104,10 @@ export class AppPublicOrganizationResolver implements Resolve<any> {
       (category: any) => category.code === code
     );
     return _.get(selectedCategory, '[0].terms', []);
+  }
+
+  private handleError(error: any, message: string) {
+    const errorMsg = _.get(error, 'error.params.errmsg', message);
+    this.snackBar.open(errorMsg, 'X', { duration: 20000, panelClass: ['error'] });
   }
 }
