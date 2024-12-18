1191397a241dafab5f67aeef4d280d0b1d632463
Afzal84 <mohd.afzal@tarento.com>
2024-12-18 13:15:11 +0530

    Fixed:faq page filter Issue

--- src/app/pipes/filter-search/filter-search.pipe.ts
+++ src/app/pipes/filter-search/filter-search.pipe.ts
@@ -9,11 +9,11 @@ export class FilterSearchPipe implements PipeTransform {
         if (!items) { return [] }
         if (!searchText) { return items }
         const searchTextLowerCase = searchText ? searchText.toLowerCase() : ''
-        return items.filter(it => {
-            if (it && it.name) {
+        return items?.filter(it => {
+            if (it && it?.name) {
                 return it.name.toLowerCase().includes(searchTextLowerCase)
-            } else if (it && it.title) {
-                return it.title?.toLowerCase().includes(searchTextLowerCase)
+            } else if (it && it?.title) {
+                return it.title.toLowerCase().includes(searchTextLowerCase)
             }
         })
     }
