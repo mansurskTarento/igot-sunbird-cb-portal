440e1b7b368413bfd28859b38ff435c55b0a64cd
Venkat Kandagaddala <venky.kandagaddala@gmail.com>
2025-02-27 17:37:56 +0530

    APi integratin to fetch initial events

--- project/ws/app/src/lib/routes/events/routes/view-all/view-all.component.ts
+++ project/ws/app/src/lib/routes/events/routes/view-all/view-all.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NsContent } from '@sunbird-cb/utils-v2';
+import { EventService } from '../../services/events.service';
+import * as _ from 'lodash'
 
 @Component({
   selector: 'ws-app-view-all',
@@ -10,7 +14,10 @@ export class ViewAllComponent {
   titles: any = []
   facetsData: any
   selectedFilters: any = {}
-  constructor() {
+  contentDataList: any = []
+  contnet: any = [
+  ]
+  constructor(private activateRoute: ActivatedRoute, private eventSvc: EventService,) {
     this.titles = [
       { title: 'Events', url: '/app/event-hub/home', disableTranslate: true, icon: 'event' },
       { title: 'Recommended Events', url: `none`, icon: '' },
@@ -116,7 +123,65 @@ export class ViewAllComponent {
   }
 
   ngOnInit() {
+    this.activateRoute.queryParamMap.subscribe((data: any) => {
+      if (data.params.resourceType) {
+        this.selectedFilters['resourceType'] = [data.params.resourceType]
+      }
+    })
+    console.log("selectedFilters", this.selectedFilters)
+    this.contentDataList = this.transformSkeletonToWidgets(this.contnet)
+    this.fetchData()
+  }
 
+  generateRequestBoday() {
+    let requestBody: any = {
+      locale: [
+        'en',
+      ],
+      query: '',
+      request: {
+        query: '',
+        filters: {
+          status: ['Live'],
+          contentType: 'Event',
+          category: 'Event',
+        },
+        sort_by: {
+          startDate: 'desc',
+        },
+        limit: 500,
+      },
+    }
+    if (this.selectedFilters && this.selectedFilters.resourceType) {
+      requestBody = {
+        ...requestBody,
+        request: {
+          ...requestBody.request,
+          filters: {
+            ...requestBody.request.filters,
+            resourceType: this.selectedFilters.resourceType,
+          },
+        },
+      };
+    }
+    console.log("this.selectedFilters.resourceType ", requestBody)
+    return requestBody
+  }
+
+  fetchData() {
+    const requestBody = this.generateRequestBoday()
+    this.eventSvc.getEventsList(requestBody).subscribe((resp: any) => {
+      const response = _.get(resp, 'result.Event', [])
+      if (response.length) {
+        this.contentDataList = this.transformContentsToWidgets(response, {})
+      } else {
+        this.contentDataList = this.transformContentsToWidgets([], {})
+      }
+      console.log("contentDataList ", this.contentDataList)
+    }, error => {
+      console.log("error", error)
+      this.contentDataList = this.transformContentsToWidgets([], {})
+    })
   }
 
   returnZero() {
@@ -157,6 +222,47 @@ export class ViewAllComponent {
 
   clearAll() {
     this.selectedFilters = {}
+  }
+
+  private transformSkeletonToWidgets(
+    strip: any
+  ) {
+    return [1, 2, 3, 4, 5, 6].map(_content => ({
+      widgetType: 'card',
+      widgetSubType: 'cardContent',
+      widgetHostClass: 'mb-2',
+      widgetData: {
+        cardSubType: strip.viewMoreUrl && strip.viewMoreUrl.loaderConfig
+          && strip.viewMoreUrl.loaderConfig.cardSubType || 'card-event-v2-skeleton',
+        cardCustomeClass: strip.customeClass ? strip.customeClass : 'card-resource-container-small',
+      },
+    }))
+  }
+
+  private transformContentsToWidgets(
+    contents: NsContent.IContent[],
+    strip: any,
+  ) {
+    return (contents || []).map((content, idx) => ({
+      widgetType: 'card',
+      widgetSubType: 'cardContent',
+      widgetHostClass: 'mb-2',
+      widgetData: {
+        content,
+        ...(content.batch && {
+          batch: content.batch,
+        }),
+        cardSubType: 'card-event-v2',
+        context: {
+          pageSection: strip.key,
+          position: idx,
+        },
+        cardCustomeClass: strip.customeClass ? strip.customeClass : 'card-resource-container-small',
+        intranetMode: strip.stripConfig && strip.stripConfig.intranetMode,
+        deletedMode: strip.stripConfig && strip.stripConfig.deletedMode,
+        contentTags: strip.stripConfig && strip.stripConfig.contentTags,
+      },
+    }))
   }
 
 
