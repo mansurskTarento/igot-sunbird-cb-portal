bb545085137f2e6ad1580ead8c5c30d202b4a4d3
Venkat Kandagaddala <venky.kandagaddala@gmail.com>
2025-02-28 20:28:49 +0530

    Events card UI

--- project/ws/app/src/lib/routes/events/routes/events/events.component.ts
+++ project/ws/app/src/lib/routes/events/routes/events/events.component.ts
@@ -138,7 +138,7 @@ export class EventsComponent implements OnInit {
     if (key) {
       const requestPayload = {
         'request': {
-          'fields' : [
+          'fields': [
             'name',
             'appIcon',
             'instructions',
@@ -180,88 +180,88 @@ export class EventsComponent implements OnInit {
             'contentType': ['Event'],
             'status': ['Live'],
           },
-            'sort_by': { 'lastUpdatedOn': 'desc' },
-            'facets': ['mimeType'],
-            'limit': 1000,
-            'offset': 0,
-          },
+          'sort_by': { 'lastUpdatedOn': 'desc' },
+          'facets': ['mimeType'],
+          'limit': 1000,
+          'offset': 0,
+        },
       }
       this.searchSrvc.fetchSearchDataByCategory(requestPayload).subscribe((response: any) => {
         if (response && response.result && response.result.Event && response.result.Event.length) {
           const allFilteredEvent = response.result.Event
 
-            const data = allFilteredEvent
-            const filterData: any = []
-            const featuredEvents: any = []
-            const curatedEvents: any = []
-            const karmayogiSaptahEvents: any = []
-            const karmayogiTalksEvents: any = []
-            Object.keys(data).forEach((index: any) => {
-              const obj = data[index]
-              // const expiryEndTimeFormat = this.customDateFormat(obj.startDate, obj.endTime)
-              const expiryStartTimeFormat = this.customDateFormat(obj.startDate, obj.startTime)
-              // const expiryEndTimeFormat = this.customDateFormat(obj.startDate, obj.endTime)
-              const floor = Math.floor
-              const hours = floor(obj.duration / 60)
-              const minutes = obj.duration % 60
-              const duration = (hours === 0) ? ((minutes === 0) ? '---' : `${minutes} minutes`) : (minutes === 0) ? (hours === 1) ?
-                `${hours} hour` : `${hours} hours` : (hours === 1) ? `${hours} hour ${minutes} minutes` :
-                `${hours} hours ${minutes} minutes`
-              const creatordata = obj.creatorDetails !== undefined ? obj.creatorDetails : []
-              const str = creatordata && creatordata.length > 0 ? creatordata.replace(/\\/g, '') : []
-              const creatorDetails = str && str.length > 0 ? JSON.parse(str) : creatordata
+          const data = allFilteredEvent
+          const filterData: any = []
+          const featuredEvents: any = []
+          const curatedEvents: any = []
+          const karmayogiSaptahEvents: any = []
+          const karmayogiTalksEvents: any = []
+          Object.keys(data).forEach((index: any) => {
+            const obj = data[index]
+            // const expiryEndTimeFormat = this.customDateFormat(obj.startDate, obj.endTime)
+            const expiryStartTimeFormat = this.customDateFormat(obj.startDate, obj.startTime)
+            // const expiryEndTimeFormat = this.customDateFormat(obj.startDate, obj.endTime)
+            const floor = Math.floor
+            const hours = floor(obj.duration / 60)
+            const minutes = obj.duration % 60
+            const duration = (hours === 0) ? ((minutes === 0) ? '---' : `${minutes} minutes`) : (minutes === 0) ? (hours === 1) ?
+              `${hours} hour` : `${hours} hours` : (hours === 1) ? `${hours} hour ${minutes} minutes` :
+              `${hours} hours ${minutes} minutes`
+            const creatordata = obj.creatorDetails !== undefined ? obj.creatorDetails : []
+            const str = creatordata && creatordata.length > 0 ? creatordata.replace(/\\/g, '') : []
+            const creatorDetails = str && str.length > 0 ? JSON.parse(str) : creatordata
 
-              const stime = obj.startTime.split('+')[0]
-              const hour = stime.substr(0, 2)
-              const min = stime.substr(2, 3)
-              const starttime = `${hour}${min}`
+            const stime = obj.startTime.split('+')[0]
+            const hour = stime.substr(0, 2)
+            const min = stime.substr(2, 3)
+            const starttime = `${hour}${min}`
 
-              const etime = obj.endTime.split('+')[0]
-              const ehour = etime.substr(0, 2)
-              const emin = etime.substr(2, 3)
-              const endtime = `${ehour}${emin}`
+            const etime = obj.endTime.split('+')[0]
+            const ehour = etime.substr(0, 2)
+            const emin = etime.substr(2, 3)
+            const endtime = `${ehour}${emin}`
 
-              const eventDataObj = {
-                event: obj,
-                eventName: obj.name,
-                eventStartTime: starttime,
-                eventEndTime: endtime,
-                eventStartDate: obj.startDate,
-                eventCreatedOn: this.allEventDateFormat(obj.createdOn),
-                eventDuration: duration,
-                eventjoined: creatorDetails.length,
-                eventThumbnail: obj.appIcon && (obj.appIcon !== null || obj.appIcon !== undefined) ?
-                  this.eventSvc.getPublicUrl(obj.appIcon) :
-                  '/assets/icons/Events_default.png',
-                pastevent: false,
-              }
-              const now = new Date()
-              const today = moment(now).format('YYYY-MM-DD HH:mm')
-              if (expiryStartTimeFormat < today) {
-                eventDataObj.pastevent = true
-              }
-              filterData.push(eventDataObj)
-              if (obj.createdFor && obj.createdFor[0] === this.departmentID) {
-                featuredEvents.push(eventDataObj)
-              }
-              this.spvOrgId = environment.spvorgID
-              if (obj.createdFor && obj.createdFor[0] === this.spvOrgId) {
-                curatedEvents.push(eventDataObj)
-              }
-              if (obj.resourceType && obj.resourceType === 'Karmayogi Saptah') {
-                karmayogiSaptahEvents.push(eventDataObj)
-              }
-              if (obj.resourceType && obj.resourceType === 'Karmayogi Talks') {
-                karmayogiTalksEvents.push(eventDataObj)
-              }
+            const eventDataObj = {
+              event: obj,
+              eventName: obj.name,
+              eventStartTime: starttime,
+              eventEndTime: endtime,
+              eventStartDate: obj.startDate,
+              eventCreatedOn: this.allEventDateFormat(obj.createdOn),
+              eventDuration: duration,
+              eventjoined: creatorDetails.length,
+              eventThumbnail: obj.appIcon && (obj.appIcon !== null || obj.appIcon !== undefined) ?
+                this.eventSvc.getPublicUrl(obj.appIcon) :
+                '/assets/icons/Events_default.png',
+              pastevent: false,
+            }
+            const now = new Date()
+            const today = moment(now).format('YYYY-MM-DD HH:mm')
+            if (expiryStartTimeFormat < today) {
+              eventDataObj.pastevent = true
+            }
+            filterData.push(eventDataObj)
+            if (obj.createdFor && obj.createdFor[0] === this.departmentID) {
+              featuredEvents.push(eventDataObj)
+            }
+            this.spvOrgId = environment.spvorgID
+            if (obj.createdFor && obj.createdFor[0] === this.spvOrgId) {
+              curatedEvents.push(eventDataObj)
+            }
+            if (obj.resourceType && obj.resourceType === 'Karmayogi Saptah') {
+              karmayogiSaptahEvents.push(eventDataObj)
+            }
+            if (obj.resourceType && obj.resourceType === 'Karmayogi Talks') {
+              karmayogiTalksEvents.push(eventDataObj)
+            }
 
-            })
-            this.alltypeEvents = filterData
-            this.karmayogiSaptahEvents = karmayogiSaptahEvents
-            this.karmayogiTalksEvents = karmayogiTalksEvents
-            this.featuredEvents = featuredEvents
-            this.curatedEvents = curatedEvents
-          }
+          })
+          this.alltypeEvents = filterData
+          this.karmayogiSaptahEvents = karmayogiSaptahEvents
+          this.karmayogiTalksEvents = karmayogiTalksEvents
+          this.featuredEvents = featuredEvents
+          this.curatedEvents = curatedEvents
+        }
 
       })
     }
@@ -325,7 +325,7 @@ export class EventsComponent implements OnInit {
   }
 
   translateHub(hubName: string): string {
-    const translationKey =  hubName
+    const translationKey = hubName
     return this.translate.instant(translationKey)
   }
 
@@ -419,7 +419,7 @@ export class EventsComponent implements OnInit {
     const min = stime.substr(2, 3)
     return `${date} ${hour}${min}`
   }
-  addCustomDateAndTime (eventData: any) {
+  addCustomDateAndTime(eventData: any) {
     const eventDate = this.eventService.customDateFormat(eventData.event.startDate, eventData.event.startTime)
     const eventendDate = this.eventService.customDateFormat(eventData.event.endDate, eventData.event.endTime)
     eventData['eventCustomStartDate'] = eventDate
@@ -472,14 +472,14 @@ export class EventsComponent implements OnInit {
         this.addCustomDateAndTime(event)
         featuredEvents.push(event)
       })
-      featuredEvents =  this.sortEvents(featuredEvents)
+      featuredEvents = this.sortEvents(featuredEvents)
     }
     if (this.allEvents['curatedEvents'] && this.allEvents['curatedEvents'].length > 0) {
       this.allEvents['curatedEvents'].forEach((event: any) => {
         this.addCustomDateAndTime(event)
         curatedEvents.push(event)
       })
-      curatedEvents =  this.sortEvents(curatedEvents)
+      curatedEvents = this.sortEvents(curatedEvents)
     }
 
     if (this.allEvents['karmayogiSaptahEvents'] && this.allEvents['karmayogiSaptahEvents'].length > 0) {
@@ -494,11 +494,11 @@ export class EventsComponent implements OnInit {
           event['isEventLive'] = true
           if (today >= event.eventCustomStartDate) {
             if (event.recordedLinks && event.recordedLinks.length > 0) {
-              event['isEventLive']  = false
+              event['isEventLive'] = false
             }
           }
         } else if (today >= event.eventCustomEndDate) {
-          event['isEventLive']  = false
+          event['isEventLive'] = false
           if (moment(today).isAfter(event.eventCustomEndDate) && moment(today).isAfter(event.eventCustomStartDate)) {
             event['isEventPast'] = true
           }
@@ -520,7 +520,7 @@ export class EventsComponent implements OnInit {
       liveEvents = this.sortEventsAsc(liveEvents)
       futureEvents = this.sortEventsAsc(futureEvents)
       pastEvents = this.sortEvents(pastEvents)
-      karmayogiSaptahEvents  = [...liveEvents, ...futureEvents, ...pastEvents]
+      karmayogiSaptahEvents = [...liveEvents, ...futureEvents, ...pastEvents]
 
     }
     if (this.allEvents['karmayogiTalksEvents'] && this.allEvents['karmayogiTalksEvents'].length > 0) {
@@ -535,11 +535,11 @@ export class EventsComponent implements OnInit {
           event['isEventLive'] = true
           if (today >= event.eventCustomStartDate) {
             if (event.recordedLinks && event.recordedLinks.length > 0) {
-              event['isEventLive']  = false
+              event['isEventLive'] = false
             }
           }
         } else if (today >= event.eventCustomEndDate) {
-          event['isEventLive']  = false
+          event['isEventLive'] = false
           if (moment(today).isAfter(event.eventCustomEndDate) && moment(today).isAfter(event.eventCustomStartDate)) {
             event['isEventPast'] = true
           }
@@ -561,7 +561,7 @@ export class EventsComponent implements OnInit {
       liveEvents = this.sortEventsAsc(liveEvents)
       futureEvents = this.sortEventsAsc(futureEvents)
       pastEvents = this.sortEvents(pastEvents)
-      karmayogiTalksEvents  = [...liveEvents, ...futureEvents, ...pastEvents]
+      karmayogiTalksEvents = [...liveEvents, ...futureEvents, ...pastEvents]
 
     }
 
@@ -595,14 +595,14 @@ export class EventsComponent implements OnInit {
     return eventData.sort((a: any, b: any) => {
       const firstDate: any = new Date(a.eventCustomStartDate)
       const secondDate: any = new Date(b.eventCustomStartDate)
-      return  secondDate > firstDate  ? 1 : -1
+      return secondDate > firstDate ? 1 : -1
     })
   }
   sortEventsAsc(eventData: any) {
     return eventData.sort((a: any, b: any) => {
       const firstDate: any = new Date(a.eventCustomStartDate)
       const secondDate: any = new Date(b.eventCustomStartDate)
-      return  secondDate < firstDate  ? 1 : -1
+      return secondDate < firstDate ? 1 : -1
     })
   }
 
@@ -669,7 +669,7 @@ export class EventsComponent implements OnInit {
     if (environment && environment.spvorgID) {
       orgId = environment.spvorgID
     }
-    const widgetData: any =  await this.eventSvc.getKeySpeakerJson().catch(_error => {})
+    const widgetData: any = await this.eventSvc.getKeySpeakerJson().catch(_error => { })
     this.keySpeakerEvents = widgetData && widgetData['keySpeakersEvents'] || []
     if (this.keySpeakerEvents && this.keySpeakerEvents.strips && this.keySpeakerEvents.strips.length) {
       if (this.keySpeakerEvents.strips[0] &&
@@ -707,5 +707,9 @@ export class EventsComponent implements OnInit {
   }
   translateLabels(label: string, type: any) {
     return this.langtranslations.translateActualLabel(label, type, '')
+  }
+
+  viewAll() {
+    this.router.navigate(['/app/event-hub/view-all'], { queryParams: { resourceType: 'Karmayogi Talks' } })
   }
 }
