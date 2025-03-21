33215264a0ca3797490156448f6ab955dbdbedc5
bharathravi-in <bharathravi.in@gmail.com>
2025-03-21 13:07:17 +0530

    changes for new landing page

--- project/ws/app/src/lib/routes/discuss-v2/discuss-v2.module.ts
+++ project/ws/app/src/lib/routes/discuss-v2/discuss-v2.module.ts
@@ -4,7 +4,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { DiscussV2RoutingModule } from './discuss-v2-routing.module';
 import { DiscussV2HomeComponent } from './routes/discuss-v2-home/discuss-v2-home.component';
 import { WidgetDiscussionv2Module,DiscussionV2Module,WidgetTopicsAllModule,
-  WidgetCommunitySearchModule,WidgetCommunityHomeModule,WidgetDiscussionv2HomeModule,TrendingDiscussionsModule,ShortcutsModule, WidgetPostdetailsModule } from '@sunbird-cb/discussion-v2';
+  WidgetCommunitySearchModule,WidgetCommunityHomeModule,WidgetDiscussionv2HomeModule,TrendingDiscussionsModule,ShortcutsModule, WidgetPostdetailsModule, 
+  WidgetDiscussionv2LandingPageModule} from '@sunbird-cb/discussion-v2';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 import { PostDetailsComponent } from './routes/post-details/post-details.component';
 import { CommunityDetailsHomeComponent } from './routes/community-details-home/community-details-home.component';
@@ -34,7 +35,8 @@ import { TopicsAllComponent } from './routes/topics-all/topics-all.component';
     TrendingDiscussionsModule,
     WidgetCommunityHomeModule,
     WidgetCommunitySearchModule,
-    WidgetTopicsAllModule
+    WidgetTopicsAllModule,
+    WidgetDiscussionv2LandingPageModule
   ]
 })
 export class DiscussV2Module { }
