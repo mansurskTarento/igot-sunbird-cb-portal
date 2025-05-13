da2fe3ed6e25e8c7fb1193f4bc152f6f5b1f675d
mansurskTarento <mansur.shaik@tarento.com>
2025-05-13 22:31:44 +0530

    wip fixing: KB-9666 Blended Program Feedback Form Submit issue.

--- library/ws-widget/collection/src/lib/player-survey/player-survey.module.ts
+++ library/ws-widget/collection/src/lib/player-survey/player-survey.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { ReactiveFormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { BtnFullscreenModule } from '../btn-fullscreen/btn-fullscreen.module'
 import { PlayerSurveyComponent } from './player-survey.component'
 import { MicroSurveyModule } from '@sunbird-cb/micro-surveys'
@@ -12,8 +12,14 @@ import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy
 import { MatLegacyMenuModule as MatMenuModule } from '@angular/material/legacy-menu'
 import { MatLegacySliderModule as MatSliderModule } from '@angular/material/legacy-slider'
 import { MatToolbarModule } from '@angular/material/toolbar'
+import { SurveyFormQuestionComponent } from '@ws/app/src/lib/routes/app-toc/components/survey-form-question/survey-form-question.component'
+import { MatLegacyRadioModule as MatRadioModule } from '@angular/material/legacy-radio'
+import { MatLegacySelectModule as MatSelectModule } from '@angular/material/legacy-select'
+import { MatLegacyCheckboxModule as MatCheckboxModule } from '@angular/material/legacy-checkbox'
+
+
 @NgModule({
-    declarations: [PlayerSurveyComponent],
+    declarations: [PlayerSurveyComponent, SurveyFormQuestionComponent],
     imports: [
         CommonModule,
         MatInputModule,
@@ -24,9 +30,13 @@ import { MatToolbarModule } from '@angular/material/toolbar'
         MatSliderModule,
         MatToolbarModule,
         ReactiveFormsModule,
+        FormsModule,
         BtnFullscreenModule,
         MatInputModule,
         MicroSurveyModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatCheckboxModule,
         TranslateModule.forChild(),
     ],
     exports: [PlayerSurveyComponent]
