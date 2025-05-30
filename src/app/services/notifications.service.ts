5a895ea33322b0b85a857f42c925252ad7b406fe
Venkat Kandagaddala <venky.kandagaddala@gmail.com>
2025-05-30 18:18:28 +0530

    Notifications service

---
+++ src/app/services/notifications.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Observable, Subject } from 'rxjs'
+import * as _ from 'lodash'
+const API_END_POINTS = {
+  NOTIFICATIONS_COUNT: `apis/proxies/v8/v1/notifications/unread/count`,
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class NotificationsService {
+  closeDialogPop = new Subject()
+  nofificationsCount = new Subject()
+  constructor(private http: HttpClient) { }
+
+  getNotificationsData(): Observable<any> {
+    return this.http.get(API_END_POINTS.NOTIFICATIONS_COUNT)
+  }
+
+
+}
