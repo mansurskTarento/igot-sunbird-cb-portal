74b6ccf73322b88db093f6081584f2afd294ea48
Venkat Kandagaddala <venky.kandagaddala@gmail.com>
2025-06-04 10:51:16 +0530

    Fixed the register page issue

--- src/app/component/app-nav-bar/app-nav-bar.component.ts
+++ src/app/component/app-nav-bar/app-nav-bar.component.ts
@@ -216,8 +216,10 @@ export class AppNavBarComponent implements OnInit, OnChanges, OnDestroy {
     if (this.configSvc.unMappedUser && this.configSvc.unMappedUser.identifier) {
       this.getMyCount()
     }
-    this.myNotificationsSubscription = this.libNotificationsService._unreadCount.subscribe(() => {
-      this.getMyCount()
+    this.myNotificationsSubscription = this.libNotificationsService._unreadCount.subscribe((res: boolean) => {
+      if (res === true) {
+        this.getMyCount()
+      }
     })
   }
 
