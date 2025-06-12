800c267e39161313b841fcaeeb9e9c8c5a2f3957
bharathravi-in <117885618+bharathravi-in@users.noreply.github.com>
2025-06-12 11:44:10 +0530

    update progress 1 disabled (#836)

--- library/ws-widget/collection/src/lib/player-survey/player-survey.component.ts
+++ library/ws-widget/collection/src/lib/player-survey/player-survey.component.ts
@@ -85,7 +85,7 @@ export class PlayerSurveyComponent extends WidgetBaseComponent
       customizedHeader: {},
     }
     this.widgetData.disableTelemetry = false
-    this.updateProgress(1)
+    // this.updateProgress(1)
 
     if (!this.widgetData.disableTelemetry) {
       this.runnerSubs = interval(30000).subscribe(_ => {
