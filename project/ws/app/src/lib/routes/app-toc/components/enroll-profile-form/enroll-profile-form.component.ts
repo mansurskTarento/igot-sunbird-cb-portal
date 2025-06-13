786b826193d6f521f95ad87aa93d041a17424df4
Afzal84 <mohd.afzal@tarento.com>
2025-06-13 19:49:07 +0530

    updated code for blended enrollment form

--- project/ws/app/src/lib/routes/app-toc/components/enroll-profile-form/enroll-profile-form.component.ts
+++ project/ws/app/src/lib/routes/app-toc/components/enroll-profile-form/enroll-profile-form.component.ts
@@ -861,19 +861,19 @@ export class EnrollProfileFormComponent implements OnInit {
         if (fieldControl) {
           if (this.pDesignation) {
             fieldControl.patchValue(this.pDesignation)
-            if (this.profileFormType === 'Available user filled iGOT profile') {
-              this.canShowDesignation = true
-              this.showDesignation = true
-            }
+            // if (this.profileFormType === 'Available user filled iGOT profile') {
+            //   this.canShowDesignation = true
+            //   this.showDesignation = true
+            // }
           } else {
             if (this.userProfileObject.profileDetails.professionalDetails &&
               this.userProfileObject.profileDetails.professionalDetails.length &&
               this.userProfileObject.profileDetails.professionalDetails[0].designation) {
               fieldControl.patchValue(this.userProfileObject.profileDetails.professionalDetails[0].designation)
-              if (this.profileFormType === 'Available user filled iGOT profile') {
-                this.canShowDesignation = false
-                this.showDesignation = false
-              }
+              // if (this.profileFormType === 'Available user filled iGOT profile') {
+              //   this.canShowDesignation = false
+              //   this.showDesignation = false
+              // }
             }
           }
           fieldControl.setValidators([Validators.required])
