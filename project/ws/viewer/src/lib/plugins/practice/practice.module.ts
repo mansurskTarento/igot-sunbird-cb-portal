a6883cb8aba99693bda27b213c231014bac41548
bharathravi-in <bharathravi.in@gmail.com>
2025-06-27 16:41:24 +0530

    removed content toc module

--- project/ws/viewer/src/lib/plugins/practice/practice.module.ts
+++ project/ws/viewer/src/lib/plugins/practice/practice.module.ts
@@ -9,8 +9,7 @@ import { SubmitQuizDialogComponent } from './components/submit-quiz-dialog/submi
 import { PipeDurationTransformModule, PipeLimitToModule } from '@sunbird-cb/utils-v2'
 import { SkeletonLoaderModule } from '@sunbird-cb/collection/src/lib/_common/skeleton-loader/skeleton-loader.module'
 import {
-  BtnFullscreenModule,
-  ContentTocModule
+  BtnFullscreenModule
 } from '@sunbird-cb/collection'
 import { ResultComponent } from './components/result/result.component'
 import { FillInTheBlankComponent } from './components/question/fitb/fitb.component'
@@ -92,8 +91,7 @@ import { ReactiveFormsModule } from '@angular/forms'
         SkeletonLoaderModule,
         TranslateModule,
         MatMenuModule,
-        MatSelectModule,
-        ContentTocModule,
+        MatSelectModule
         
     ],
     exports: [
