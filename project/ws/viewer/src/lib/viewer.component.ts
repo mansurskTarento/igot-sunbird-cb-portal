a93466ac272bda5f582ef7e50dbeeef9be840e25
Bharath Ravi <117885618+bharathravi-in@users.noreply.github.com>
2025-08-11 20:12:47 +0530

    remove download certificate code (#1093)

--- project/ws/viewer/src/lib/viewer.component.ts
+++ project/ws/viewer/src/lib/viewer.component.ts
@@ -267,14 +267,14 @@ export class ViewerComponent implements OnInit, OnDestroy, AfterViewChecked {
       }
     })
 
-    if (this.collectionId) {
-      if (!this.forPreview) {
-        const enrollCourseData = this.enrolledCourseData
-        if (enrollCourseData && (enrollCourseData.completionPercentage === 100 || enrollCourseData.status === 2)) {
-          this.downloadCertificate(enrollCourseData)
-        }
-      }
-    }
+    // if (this.collectionId) {
+    //   if (!this.forPreview) {
+    //     const enrollCourseData = this.enrolledCourseData
+    //     if (enrollCourseData && (enrollCourseData.completionPercentage === 100 || enrollCourseData.status === 2)) {
+    //       this.downloadCertificate(enrollCourseData)
+    //     }
+    //   }
+    // }
   }
 
   ngAfterViewChecked() {
