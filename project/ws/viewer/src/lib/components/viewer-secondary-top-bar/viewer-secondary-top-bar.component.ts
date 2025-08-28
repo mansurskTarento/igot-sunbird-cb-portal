fefb6555771409ac1285df051a2cbd27c9afc35e
Christopher B Fernandes <Christyabcd.05@gmail.com>
2025-08-28 17:11:14 +0530

    Merge pull request #1153 from christyfernandes/cbrelease-4.8.27-multilingual-new
    Cbrelease 4.8.27 multilingual new

--- project/ws/viewer/src/lib/components/viewer-secondary-top-bar/viewer-secondary-top-bar.component.ts
+++ project/ws/viewer/src/lib/components/viewer-secondary-top-bar/viewer-secondary-top-bar.component.ts
@@ -402,6 +402,7 @@ export class ViewerSecondaryTopBarComponent implements OnInit, OnDestroy {
         identifier: id,
         primaryCategory: this.collectionType,
         courseCategory: this.currentDataFromEnrollList.content.courseCategory,
+        collectionId: this.identifier // In case of multilingual course, redirection should happen to base collectionID
       },
     })
     dialogRef.afterClosed().subscribe(result => {
