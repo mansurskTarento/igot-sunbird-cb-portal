ea4343a887b6f55bbbf90a0b5d1444d057c6f9d9
Vishnu Bansal <vishnubansaltarento>
2025-09-02 14:53:41 +0530

    update for issue 11407

--- project/ws/app/src/lib/routes/search-v3/routes/learn-search/learn-search.component.ts
+++ project/ws/app/src/lib/routes/search-v3/routes/learn-search/learn-search.component.ts
@@ -327,7 +327,10 @@ export class LearnSearchComponent implements OnInit, OnChanges, OnDestroy {
 
   async searchCourses() {
     if(this.searchRequestCourse && this.searchRequestCourse['request'] && Object.keys(this.searchRequestCourse['request']['filters'])) {
-      this.searchRequestCourse['request']['filters']['courseCategory'] = {"!=":["pre enrolment assessment"]}
+      if(this.searchRequestCourse['request']['filters']['courseCategory'].length === 0) {
+        this.searchRequestCourse['request']['filters']['courseCategory'] = {"!=":["pre enrolment assessment"]}
+      }
+      
     }
     this.searchRequestCourse.request.query = this.statedata?.param;
     const result = await this.searchV3Service.searchCoursesv4(
