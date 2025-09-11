d6e85b9fadce6842f7c3cc1dbd31ba8d1667ad5f
Sohith Kanala <kanala.sohith@tarento.com>
2025-09-11 15:39:48 +0530

    Apar changes on show all igot and consumption package updated

--- src/app/component/cbp-side-bar/cbp-side-bar.component.ts
+++ src/app/component/cbp-side-bar/cbp-side-bar.component.ts
@@ -10,6 +10,7 @@ export class CbpSideBarComponent implements OnInit {
   @Input() cbpCount: any
   @Input() upcommingList: any
   @Input() overDueList: any
+  @Input() aparList: any
   @Input() cbpLoader: any
   @Input() cbpOriginalData: any
   @Output()
