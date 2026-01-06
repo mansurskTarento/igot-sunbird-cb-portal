8985497bdf9dbea73018f440595e36a81374a6f0
Bharath R <bharathravi.in@gmail.com>
2026-01-06 15:07:55 +0530

    error for value

--- project/ws/app/src/lib/routes/gyaan-karmayogi/pipes/replace-nbsp-text.pipe.ts
+++ project/ws/app/src/lib/routes/gyaan-karmayogi/pipes/replace-nbsp-text.pipe.ts
@@ -6,10 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core'
 export class ReplaceNbspTextPipe implements PipeTransform {
 
   transform(value: any): any {
-    if (value.includes('&nbsp;')) {
+    if (value && value.includes('&nbsp;')) {
       return value.replace(/&nbsp;/g, ' ')
     }
-      return value
+    return value
   }
 
 }
