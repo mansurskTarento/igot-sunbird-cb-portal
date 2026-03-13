55bf4f5e9fdbefcd6fc16dc70544c1e6969f36ae
Sohith Kanala <kanala.sohith@tarento.com>
2026-03-13 17:44:20 +0530

    Added banner and support form html

--- src/app/header/header/header.component.ts
+++ src/app/header/header/header.component.ts
@@ -5,6 +5,11 @@ import { MobileAppsService } from '../../services/mobile-apps.service'
 import {
   ValueService,
 } from '@sunbird-cb/utils-v2'
+import { MatLegacyDialog as MatDialog } from '@angular/material/legacy-dialog'
+import { HttpClient } from '@angular/common/http'
+import { DomSanitizer } from '@angular/platform-browser'
+import { DialogBoxComponent as ZohoDialogComponent } from '@ws/app/src/lib/routes/profile-v3/components/dialog-box/dialog-box.component'
+import { ZohoFormService } from './zoho-form.service'
 @Component({
   selector: 'ws-header',
   templateUrl: './header.component.html',
@@ -16,17 +21,28 @@ export class HeaderComponent implements OnInit {
   showNavbar = true
   widgetData = {}
   mobileTopHeaderVisibilityStatus = true
+  zohoHtml: any
+  zohoUrl: any = '/assets/static-data/support-html/zoho_karmayogi_form.html'
   @Input() mode: any
   @Input() headerFooterConfigData: any
   @Input() showHubs = false
   constructor(
     private valueSvc: ValueService,
     public headerService: HeaderService,
-    public mobileAppsService: MobileAppsService) { }
+    public mobileAppsService: MobileAppsService,
+    public dialog: MatDialog,
+    private http: HttpClient,
+    private sanitizer: DomSanitizer,
+    private zohoFormService: ZohoFormService) { }
 
   ngOnInit() {
     this.headerService.showNavbarDisplay$.pipe(delay(500)).subscribe(display => {
       this.showNavbar = display
+    })
+
+    // Load Zoho form HTML
+    this.http.get(this.zohoUrl, { responseType: 'text' }).subscribe(res => {
+      this.zohoHtml = this.sanitizer.bypassSecurityTrustHtml(res)
     })
 
     // tslint:disable-next-line: whitespace
@@ -76,5 +92,44 @@ export class HeaderComponent implements OnInit {
   hideMobileTopHeader() {
     this.mobileTopHeaderVisibilityStatus = false
     this.mobileAppsService.mobileTopHeaderVisibilityStatus.next(this.mobileTopHeaderVisibilityStatus)
+  }
+
+  openSupportForm(): void {
+    this.dialog.open(ZohoDialogComponent, {
+      width: 'auto',
+      height: '100vh',
+      maxWidth: '100vw',
+      position: {
+        top: '0',
+        right: '0'
+      },
+      panelClass: 'right-side-dialog',
+      data: {
+        view: 'zohoform',
+        value: this.zohoHtml,
+      },
+    })
+    setTimeout(() => {
+      this.initializeZohoForm()
+    }, 300)
+  }
+
+  private initializeZohoForm(): void {
+    try {
+      // Expose all form handlers to window for HTML event bindings
+      (window as any).handleIssueType = (sel: any) => { this.zohoFormService.handleIssueTypeChange(sel); return true }
+      (window as any).toggleCentreState = (sel: any) => { this.zohoFormService.toggleCentreState(sel); return true }
+      (window as any).toggleAIS = (sel: any) => { this.zohoFormService.toggleAIS(sel); return true }
+      (window as any).zsRenderBrowseFileAttachment = (filePath: string, element: any) => { this.zohoFormService.handleFileAttachment(filePath, element); return true }
+      (window as any).zsRegenerateCaptcha = () => { this.zohoFormService.loadCaptcha(); return true }
+      (window as any).zsResetWebForm = (id: string) => { this.zohoFormService.resetForm(id); return true }
+      (window as any).zsValidateMandatoryFields = () => { return this.zohoFormService.validateAndSubmitForm() }
+      
+      this.zohoFormService.loadCaptcha()
+      this.zohoFormService.patchUserDataFromConfig()
+    } catch (error) {
+      console.error('Error initializing Zoho form:', error)
+      this.zohoFormService.loadCaptcha()
+    }
   }
 }
