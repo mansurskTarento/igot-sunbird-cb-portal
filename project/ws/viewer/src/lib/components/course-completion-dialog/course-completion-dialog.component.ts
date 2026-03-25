59054ff7ff0f3a8dc710c380de8491db4cb03c89
SaloniTarento <saloni.chhaperwal@tarento.com>
2026-03-25 20:28:27 +0530

    fixed bug KB-13637

--- project/ws/viewer/src/lib/components/course-completion-dialog/course-completion-dialog.component.ts
+++ project/ws/viewer/src/lib/components/course-completion-dialog/course-completion-dialog.component.ts
@@ -37,7 +37,17 @@ export class CourseCompletionDialogComponent implements OnInit {
     const badgeDetails = this.data?.baseContentReadData?.badgeDetails_v1
 
     if (badgeDetails && badgeDetails.length) {
-      this.badge = badgeDetails[0]
+      const badge = badgeDetails[0]
+      console.log('badgeEarningDateTime', badge.badgeEarningDateTime)
+
+      const now = Date.now()
+
+      if (!badge.badgeEarningDateTime || badge.badgeEarningDateTime > now) {
+        this.badge = badge
+      } else {
+        this.badge = null
+      }
+
     }
     const app: any = document.getElementById('viewer-conatiner-backdrop')
     if (app) {
