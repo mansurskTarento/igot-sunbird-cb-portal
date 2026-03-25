b2b3a6a9b3cc95f0cad840164921b9e3ea07e31b
Bharath R <bharathravi.in@gmail.com>
2026-03-25 11:47:41 +0530

    KB-13494 changes for Sadhana saptha

--- project/ws/app/src/lib/routes/national-learning-week/karmayogi-saptah/karmayogi-saptah.component.ts
+++ project/ws/app/src/lib/routes/national-learning-week/karmayogi-saptah/karmayogi-saptah.component.ts
@@ -3,11 +3,11 @@ import { ActivatedRoute } from '@angular/router'
 import { ConfigurationsService } from '@sunbird-cb/utils-v2'
 
 @Component({
-  selector: 'ws-app-national-learning-week-microsite',
-  templateUrl: './national-learning-week-microsite.component.html',
-  styleUrls: ['./national-learning-week-microsite.component.scss'],
+  selector: 'ws-app-karmayogi-saptah',
+  templateUrl: './karmayogi-saptah.component.html',
+  styleUrls: ['./karmayogi-saptah.component.scss'],
 })
-export class NationalLearningWeekMicrositeComponent implements OnInit {
+export class KarmayogiSaptahComponent implements OnInit {
 
   sectionList: any = []
   nwlConfig: any
@@ -25,10 +25,10 @@ export class NationalLearningWeekMicrositeComponent implements OnInit {
       this.sectionList = this.route.snapshot.data.formData.data.result.form.data.sectionList
     }
     if (this.route.snapshot.data
-        && this.route.snapshot.data.configData
-        && this.route.snapshot.data.configData.data
-        && this.route.snapshot.data.configData.data.nationalLearningWeek
-      ) {
+      && this.route.snapshot.data.configData
+      && this.route.snapshot.data.configData.data
+      && this.route.snapshot.data.configData.data.nationalLearningWeek
+    ) {
       this.nwlConfig = this.route.snapshot.data.configData.data.nationalLearningWeek
     }
     // tslint:disable-next-line:no-console
--- project/ws/app/src/lib/routes/national-learning-week/national-learning-week-routing.module.ts
+++ project/ws/app/src/lib/routes/national-learning-week/national-learning-week-routing.module.ts
@@ -1,22 +1,36 @@
 import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
-import { NationalLearningWeekMicrositeComponent } from './national-learning-week-microsite/national-learning-week-microsite.component'
+import { KarmayogiSaptahComponent } from './karmayogi-saptah/karmayogi-saptah.component'
 import { NationalLearningWeekFormService } from './services/national-learning-week-form.service'
 import { NationalLearningConfigService } from './services/national-learning-config.service'
+import { SadhanaSaptahComponent } from './sadhana-saptah/sadhana-saptah.component'
 
 const routes: Routes = [
     {
-        path: '',
-        component: NationalLearningWeekMicrositeComponent,
+        path: 'karmayogi-saptah',
+        component: KarmayogiSaptahComponent,
         data: {
             module: 'National Learning Week',
-            pageId: 'app/learn/karmayogi-saptah',
+            pageId: 'app/learn/nlw/karmayogi-saptah',
+            pageKey: 'karmayogi-saptah'
         },
         resolve: {
             formData: NationalLearningWeekFormService,
             configData: NationalLearningConfigService,
         },
     },
+    {
+        path: 'sadhana-saptah',
+        component: SadhanaSaptahComponent,
+        data: {
+            module: 'National Learning Week',
+            pageId: 'app/learn/nlw/sadhana-saptah',
+            pageKey: 'sadhana-saptah',
+        },
+        resolve: {
+            formData: NationalLearningWeekFormService
+        },
+    }
 ]
 
 @NgModule({
