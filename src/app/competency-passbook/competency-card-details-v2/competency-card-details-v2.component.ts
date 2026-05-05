088b12397ddc3cd8a4e9043abf56d67df6c3bcd3
venkykandagaddala <venky.kandagaddala@gmail.com>
2026-05-05 16:44:33 +0530

    PDF viewer

--- src/app/competency-passbook/competency-card-details-v2/competency-card-details-v2.component.ts
+++ src/app/competency-passbook/competency-card-details-v2/competency-card-details-v2.component.ts
@@ -15,7 +15,7 @@ import { MatLegacyDialog as MatDialog } from '@angular/material/legacy-dialog'
 import { CertificateDialogComponent } from '@sunbird-cb/collection/src/lib/_common/certificate-dialog/certificate-dialog.component'
 import { MatSnackBar } from '@angular/material/snack-bar'
 import { CertificateViewPopupComponent } from '../../../../project/ws/app/src/lib/routes/profile-v2/components/profile-revamp/certificate-view-popup/certificate-view-popup.component'
-
+import { NlwCertificateDialogComponent } from '@sunbird-cb/consumption'
 @Component({
   selector: 'ws-competency-card-details-v2',
   templateUrl: './competency-card-details-v2.component.html',
@@ -458,6 +458,27 @@ export class CompetencyCardDetailsV2Component implements OnInit, OnDestroy {
           certificateUrl: url
         },
         disableClose: true,
+        autoFocus: false,
+      })
+    }
+  }
+
+  openPDF(content: any): void {
+    if (content?.certificateId) {
+      let dialogData = {
+        pdfZoom: 'FitH',
+        type: 'PDF',
+        action: 'view',
+        title: content.certificateId.split("/")[-1] || content?.name || 'PDF Document',
+        url: content.certificateId,
+      }
+      let dialogWidth = '700px'
+      dialogWidth = window.innerWidth <= 768 ? '90vw' : '80vw'
+      this.dialog.open(NlwCertificateDialogComponent, {
+        data: dialogData,
+        panelClass: 'nlw-experience-dialog-container',
+        maxWidth: '95vw',
+        width: dialogWidth,
         autoFocus: false,
       })
     }
