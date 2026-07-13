a3b4f2b129a9eac418ed6735477afa06b018cc53
mansurskTarento <mansur.shaik@tarento.com>
2026-07-13 20:43:07 +0530

    ui ux revamp ui changes

--- src/app/component/root/root.component.ts
+++ src/app/component/root/root.component.ts
@@ -127,13 +127,21 @@ export class RootComponent implements OnInit, AfterViewInit, AfterViewChecked {
       this.headerFooterConfigData = this.configSvc.headerFooterConfigData
       this.showFooter = true
     }
-    this.getLeftNavBarConfiguration().subscribe((sectionData: any) => {
-      this.menuBarDetails = sectionData?.data
+    if (this.configSvc.instanceConfig && this.configSvc.instanceConfig.leftNavBar) {
+      this.menuBarDetails = this.configSvc.instanceConfig.leftNavBar
       if (this.menuBarDetails) {
         this.setAchivements()
         this.setOtherPortals()
       }
-    })
+    } else {
+      this.getLeftNavBarConfiguration().subscribe((sectionData: any) => {
+        this.menuBarDetails = sectionData?.data
+        if (this.menuBarDetails) {
+          this.setAchivements()
+          this.setOtherPortals()
+        }
+      })
+    }
 
     if (window.location.pathname.includes('/public/home')
       || window.location.pathname.includes('/public/toc/')
