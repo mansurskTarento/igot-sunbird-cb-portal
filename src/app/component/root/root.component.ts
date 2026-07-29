b1a81a1a167bf34113e7cc0c7b6d7f3da9f8b418
srikanthmutte <srikanth.mutte@tarento.com>
2026-07-29 15:45:33 +0530

    if no data redirect to karmapoints

--- src/app/component/root/root.component.ts
+++ src/app/component/root/root.component.ts
@@ -891,7 +891,27 @@ export class RootComponent implements OnInit, AfterViewInit, AfterViewChecked {
   }
 
   viewAllAchievements() {
-    this.showKarmaLeaderboard.set(true)
+    this.homePageSvc.getLearnerLeaderboardCached().subscribe(
+      (res: any) => {
+        const results = _.get(res, 'result.result', [])
+        if (Array.isArray(results) && results.length >= 3) {
+          this.showKarmaLeaderboard.set(true)
+        } else {
+          this.navigateToKarmaPoints()
+        }
+      },
+      () => {
+        this.navigateToKarmaPoints()
+      },
+    )
+  }
+
+  private navigateToKarmaPoints() {
+    this.showKarmaLeaderboard.set(false)
+    this.openStatusUserSelection.set(false)
+    this.leftNavBarIsOpen.set(false)
+    this.navBarOpenStatusBasedOnNav.set(false)
+    this.router.navigate(['/app/person-profile/karma-points'])
   }
 
   exploreContent() {
