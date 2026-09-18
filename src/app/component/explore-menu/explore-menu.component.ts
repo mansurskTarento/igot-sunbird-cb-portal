459a9c5d3530dba2776b9b9011f71d55ea0d2ba2
mansurskTarento <mansur.shaik@tarento.com>
2026-09-18 16:52:11 +0530

    fixing telemetry empty object issue

--- src/app/component/explore-menu/explore-menu.component.ts
+++ src/app/component/explore-menu/explore-menu.component.ts
@@ -4,7 +4,9 @@ import {
   ConfigurationsService,
   EventService,
   MultilingualTranslationsService,
+  TelemetryService,
   WsEvents,
+  UtilityService
 } from '@sunbird-cb/utils-v2'
 import { CommonDataService } from '../../services/common-data.service'
 
@@ -38,6 +40,8 @@ export class ExploreMenuComponent implements OnInit {
     private eventSvc: EventService,
     private commonDataSvc: CommonDataService,
     private langtranslations: MultilingualTranslationsService,
+    private utilitySvc: UtilityService,
+    private telemetrySvc: TelemetryService
   ) { }
 
   ngOnInit() {
@@ -197,6 +201,8 @@ export class ExploreMenuComponent implements OnInit {
   }
 
   private raiseTelemetry(id: string, subType: string = '') {
+    this.telemetrySvc.sendEmptyObjectForNextInteract()
+    this.utilitySvc.setRouteData([{ module: 'Home', pageId: 'page/home' }])
     const eData: any = {
       id,
       type: WsEvents.EnumInteractTypes.CLICK,
